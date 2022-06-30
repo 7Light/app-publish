792c436192b2f0265c4e314c66cc27366ee13486
xiongfengbo <xiongfengbo@huawei.com>
2022-06-30 16:09:09 +0800

    mindspore

--- src/main/java/com/huawei/publish/PublishVerifyController.java
+++ src/main/java/com/huawei/publish/PublishVerifyController.java
@@ -88,8 +88,7 @@ public class PublishVerifyController {
                 }
                 String fileName = file.getName();
                 if (!StringUtils.isEmpty(file.getUrl()) && file.getUrl().startsWith("http")) {
-                    verifyService.execCmd("wget --no-check-certificate -P " + tempDirPath
-                            + " " + file.getUrl() + fileName);
+                    fileDownloadService.downloadHttpUrl(file.getUrl(), tempDirPath, fileName);
                     if (!fileName.endsWith(".sha256") && !fileName.endsWith(".asc")) {
                         file.setSha256(fileDownloadService.getContent(file.getUrl() + fileName + ".sha256"));
                     }
