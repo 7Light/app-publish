18f7b309ff72db20939cd6016954dbdca2e3ec36
chentao <1203467115@qq.com>
2023-03-22 18:00:13 +0800

    新增鉴权功能

---
+++ src/main/java/com/huawei/publish/enums/AppConst.java
@@ -0,0 +1,11 @@
+package com.huawei.publish.enums;
+
+/**
+ * @author chentao
+ */
+public class AppConst {
+    /**
+     * access_token
+     */
+    public static final String ACCESS_TOKEN = "access_token";
+}
