b664f531ba92c5cf14cff0391d6ac74271f3095e
chentao <1203467115@qq.com>
2023-06-01 19:44:35 +0800

    请求头中参数带_，在nginx转发后会丢失

--- src/main/java/com/huawei/publish/enums/AppConst.java
+++ src/main/java/com/huawei/publish/enums/AppConst.java
@@ -7,7 +7,7 @@ public class AppConst {
     /**
      * access_token
      */
-    public static final String ACCESS_TOKEN = "access_token";
+    public static final String ACCESS_TOKEN = "accessToken";
 
     /**
      * exists
