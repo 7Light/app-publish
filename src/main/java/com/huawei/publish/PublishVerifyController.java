5e10732360c6e29d178ad0a527cbc17d772703c6
chentao <1203467115@qq.com>
2023-11-28 21:07:17 +0800

    注释限制rpm包上传到正式发布库

--- src/main/java/com/huawei/publish/PublishVerifyController.java
+++ src/main/java/com/huawei/publish/PublishVerifyController.java
@@ -138,8 +138,9 @@ public class PublishVerifyController {
                     verifyService.execCmd("ssh -i /var/log/ssh_key/private.key -o StrictHostKeyChecking=no root@"
                         + publishObject.getRemoteRepoIp() + " \"mkdir -p " + file.getTargetPath() + "\"");
                 }
-                String outPut = verifyService.execCmd("scp -i /var/log/ssh_key/private.key -o StrictHostKeyChecking=no " + tempDirPath
-                        + fileName + " root@" + publishObject.getRemoteRepoIp() + ":" + file.getTargetPath() + AppConst.SLASH + fileName);
+                String outPut = "success";
+//                        verifyService.execCmd("scp -i /var/log/ssh_key/private.key -o StrictHostKeyChecking=no " + tempDirPath
+//                        + fileName + " root@" + publishObject.getRemoteRepoIp() + ":" + file.getTargetPath() + AppConst.SLASH + fileName);
                 if (!StringUtils.isEmpty(outPut)) {
                     file.setPublishResult("failed");
                     result.setResult("fail");
